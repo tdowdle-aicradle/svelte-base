f39e0634328bf50090ec977e47b1970c68331f50
Trevor <tdowdle@teambanjo.com>
2021-01-14 15:38:35 -0700

    prod build fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,19 @@
+
+
+// This gives purge compatibility with Svelte class: directives
+// This could break with future Tailwindcss updates - See https://github.com/tailwindlabs/tailwindcss/discussions/1731
+const { tailwindExtractor } = require("tailwindcss/lib/lib/purgeUnusedStyles");
+
+
 const purgecss = require("@fullhuman/postcss-purgecss")({
     content: ["./src/**/*.html", "./src/**/*.svelte"],
-
     whitelistPatterns: [/svelte-/],
-
-    defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    defaultExtractor: (content) => [
+        ...tailwindExtractor(content),
+        // Match Svelte class: directives (https://github.com/tailwindlabs/tailwindcss/discussions/1731)
+        ...[...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
+    ],
+    keyframes: true,
 })
 
 const dev = process.env.ROLLUP_WATCH
@@ -11,3 +21,5 @@ const dev = process.env.ROLLUP_WATCH
 module.exports = {
     plugins: [require("tailwindcss"), ...(!dev ? [purgecss] : [])],
 }
+
+
